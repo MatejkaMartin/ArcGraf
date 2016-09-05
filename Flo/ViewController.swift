cbf4ee0e2ac466967ee3c5650f57727b52078c4a
Martin Prace <martinmatejka@martin-macbook-pro.local>
2016-09-05 21:52:29 +0200

    Initial Commit

---
+++ Flo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flo
+//
+//  Created by Martin Matějka on 05.09.16.
+//  Copyright © 2016 Martin Matějka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
